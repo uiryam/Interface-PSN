0e9cc0907d6a688648fcf36d7db718ba257345d6
“uiryam” <willlevil@gmail.com>
2023-11-21 20:55:52 -0400

    Conteudo da Aula Finalizado

---
+++ src/app/components/menu-bar/menu-bar-social/menu-bar-social.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar-social',
+  templateUrl: './menu-bar-social.component.html',
+  styleUrls: ['./menu-bar-social.component.css']
+})
+export class MenuBarSocialComponent {
+
+}
